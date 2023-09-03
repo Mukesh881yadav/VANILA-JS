2f7ea85021d0a078bc1c1c6a92c80d1235857bc2
Mukesh881yadav <mukesh.yadav@neosoftmail.com>
2023-09-03 11:20:56 +0530

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144553;
+let accountemial="mukesh@gmail.com";
+var accountPassword=123456789;
+accountCity="jaipur";
+let accountstate;
+
+// accountId=2323; // const not allowed  change value
+accountemial="mukki@gmail.com"
+accountPassword=987654321
+accountCity="Mumbai"
+// console.log(accountCity);
+
+// for run all variable at same time try this 
+ console.table([accountemial,accountPassword,accountCity,accountstate])
+
+/*
+prefer not use var becaause of issue in block and funtional scope
+ */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+//  primitive type
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value null means we assign value empty 
+// undefined => 
+// symbol => unique //this most use in react for unique ness there is multiple component in react 
+
+// refrence type
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
