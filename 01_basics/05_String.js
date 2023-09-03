aac311875cab5023a503ffe8ff70d53e616c0ce2
Mukesh881yadav <mukesh.yadav@neosoftmail.com>
2023-09-03 18:10:04 +0530

    String metods

---
+++ 01_basics/05_String.js
@@ -0,0 +1,22 @@
+// behind the seen this work also same invok object
+const name="Mukesh Yadav";
+const repoCount=50;
+
+console.log(name + repoCount + "value");
+
+// back ticks (string interpoletion)
+console.log(`my name is ${name}  and my repocount is ${repoCount}`);
+
+
+// her we define string as using new key word for use object of javascript 
+const gameName=new String("Mukku Bhai")
+
+// console.log(name[0])
+// console.log(name.length)
+// upper lower both are same
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// but he did't change original value due to he copy to value in  another stack memory 
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('M'));
